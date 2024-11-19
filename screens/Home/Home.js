7c249da136730ebddf25cc057c07ecc17d47d9eb
Pecuchova <jpecuchova96@gmail.com>
2024-11-19 19:31:28 +0100

    tab component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View } from "react-native";
 
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
+import Tab from "../../components/Tab/Tab";
 
 import globalStyle from "../../assets/styles/globalStyle";
 import style from "./style";
+import { horizontalScale } from "../../assets/styles/scaling";
 
 
 const Home = () => {
     return (
         <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
             <Header title={"Azzahri A."} type={1} />
-            <Button
-                title={'Donate'}
-                onPress={() => {
-                    console.log('pressed button');
-                }}
-            />
-            <Button title={'Donate'} isDisabled={true} />
+            {/* <View style={{ width: horizontalScale(130) }}> */}
+            <Tab title={"Highlight"} />
+            <Tab title={"Highlight"} isInactive={true} />
+            {/*</View> */}
         </SafeAreaView>
     );
 };
