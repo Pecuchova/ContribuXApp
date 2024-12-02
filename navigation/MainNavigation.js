1a1e13e10eb27cdc7b77700b4135db9c37733457
Pecuchova <jpecuchova96@gmail.com>
2024-12-02 20:07:06 +0100

    authenticated - non authenticated status

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -9,13 +9,22 @@ import Registration from '../screens/Registration/Registration';
 
 const Stack = createStackNavigator();
 
-const MainNavigation = () => {
+export const NonAuthenticated = () => {
     return (
         <Stack.Navigator
             initialRouteName={Routes.Login}
             screenOptions={{ header: () => null, headerShown: false }}>
             <Stack.Screen name={Routes.Login} component={Login} />
             <Stack.Screen name={Routes.Registration} component={Registration} />
+        </Stack.Navigator>
+    );
+};
+
+export const Authenticated = () => {
+    return (
+        <Stack.Navigator
+            initialRouteName={Routes.Home}
+            screenOptions={{ header: () => null, headerShown: false }}>
             <Stack.Screen name={Routes.Home} component={Home} />
             <Stack.Screen
                 name={Routes.SingleItem}
@@ -25,5 +34,3 @@ const MainNavigation = () => {
     );
 };
 
-
-export default MainNavigation;
