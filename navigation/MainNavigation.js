bbc593ada9da02ecd421bef8b618b200e51cf8ad
Pecuchova <jpecuchova96@gmail.com>
2024-12-02 18:01:52 +0100

    login screen

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,13 +2,18 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Routes } from "./Routes";
 import Home from "../screens/Home/Home";
 import SingleItem from "../screens/SingleItem/SingleItem";
+import Login from "../screens/Login/Login";
+
 
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
     return (
-        <Stack.Navigator screenOptions={{ header: () => null, headerShown: false }}>
+        <Stack.Navigator
+            initialRouteName={Routes.Login}
+            screenOptions={{ header: () => null, headerShown: false }}>
+            <Stack.Screen name={Routes.Login} component={Login} />
             <Stack.Screen name={Routes.Home} component={Home} />
             <Stack.Screen
                 name={Routes.SingleItem}
