d0f4cf8569e5c0a34c7b52e99f29c7003ed5fdec
Pecuchova <jpecuchova96@gmail.com>
2024-12-02 18:13:33 +0100

    registration screen

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import { Routes } from "./Routes";
 import Home from "../screens/Home/Home";
 import SingleItem from "../screens/SingleItem/SingleItem";
 import Login from "../screens/Login/Login";
+import Registration from '../screens/Registration/Registration';
 
 
 
@@ -14,6 +15,7 @@ const MainNavigation = () => {
             initialRouteName={Routes.Login}
             screenOptions={{ header: () => null, headerShown: false }}>
             <Stack.Screen name={Routes.Login} component={Login} />
+            <Stack.Screen name={Routes.Registration} component={Registration} />
             <Stack.Screen name={Routes.Home} component={Home} />
             <Stack.Screen
                 name={Routes.SingleItem}
